40dba04b121d92f1ff06d13c422d0888f50cad2b
Lojaxin <37296875+Lojaxin@users.noreply.github.com>
2023-09-14 08:38:36 +0000

    Deploying to gh-pages from @ Lojaxin/lojaxin.github.io@cd557d2e70529b7178297409c7571cdbec9574bf 🚀

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuItems = document.querySelectorAll(".menu li");
-
-    menuItems.forEach((item) => {
-        item.addEventListener("click", () => {
-
-            const submenu = item.querySelector("ul");
-            if (submenu) {
-                submenu.style.display = submenu.style.display === 'block' ? 'none' : "block";
-            }
-        });
+    let menuItems = document.querySelectorAll(".submenu");
+    menuItems.forEach((li) => {
+        const menuNode = li.querySelector('a');
+        if (menuNode.href.startsWith('javacript:')) {
+            menuNode.addEventListener("click", (event) => {
+                event.preventDefault();
+                const ul = menuNode.nextElementSibling;
+                if (ul) {
+                    ul.style.display = ul.style.display === 'block' ? 'none' : "block";
+                    menuNode.classList.toggle('active');
+                }
+            });
+        }
     });
-
 });
